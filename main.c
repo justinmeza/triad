f98cf98c91e064766e1e5035f956d63ce2a8f84c
Justin Meza <justin.meza@gmail.com>
2013-05-14 21:52:17 -0400

    rpc stuff

--- main.c
+++ main.c
@@ -2,19 +2,11 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "inet.h"
-#include "hash.h"
 
 #define RPC_PORT 12345
 #define COM_PORT 12346
 
-struct hash *rpc_table;
-
 void data_delete_rpc(void *DATA) { }
-
-void rpc_get_status(void)
-{
-	printf("STATUS = \n");
-}
 
 typedef struct msg {
 	char type[32];
@@ -25,6 +17,12 @@ typedef struct node {
 	int status;
 } node_t;
 
+void rpc_get_status(node_t *node, msg_t *msg)
+{
+	printf("STATUS = \n"), fflush(stdout);
+	strcpy(msg->type, "RPC_GET_STATUS_ACK");
+}
+
 int main(int argc, char **argv)
 {
 	/* set up node */
@@ -34,18 +32,12 @@ int main(int argc, char **argv)
 	char *ip = inet_lookup("localhost");
 	printf("IP is: %s\n", ip);
 
-	rpc_table = hash_create(data_delete_rpc, 7);
-
-	hash_insert(rpc_table, "RPC_GET_STATUS", &rpc_get_status);
-	void (*fn)(void) = hash_find(rpc_table, "RPC_GET_STATUS");
-	fn();
-
 	/* RPC thread */
 	int id = fork();
 	if (id == 0) {
 		inet_host_t local, remote;
-		inet_open(&local, IN_PROT_TCP, IN_ADDR_ANY, RPC_PORT);
 		while (1) {
+			inet_open(&local, IN_PROT_TCP, IN_ADDR_ANY, RPC_PORT);
 			printf("waiting for node to connect...\n"), fflush(stdout);
 			inet_accept(&remote, &local);
 			printf("accepted connection\n"), fflush(stdout);
@@ -61,9 +53,13 @@ int main(int argc, char **argv)
 					return 0;
 				}
 				else if (!strcmp(m.type, "RPC_GET_STATUS")) {
-					printf("sending status...\n"), fflush(stdout);
+					msg_t m;
+					rpc_get_status(&n, &m);
+					inet_send(&local, &remote, &m, sizeof(msg_t));
 				}
 			}
+			inet_close(&local);
+			inet_close(&remote);
 		}
 		return 0;
 	}
@@ -85,13 +81,24 @@ int main(int argc, char **argv)
 			inet_close(&local);
 			break;
 		}
+		else if (!strcmp(line, "status")) {
+			inet_open(&local, IN_PROT_TCP, IN_ADDR_ANY, COM_PORT);
+			inet_setup(&remote, IN_PROT_TCP, ip, RPC_PORT);
+			inet_connect(&local, &remote);
+			msg_t m = { "RPC_GET_STATUS" };
+			inet_send(&local, &remote, &m, sizeof(msg_t));
+			msg_t ack;
+			inet_receive(&remote, &local, &ack, sizeof(msg_t), -1);
+			if (!strcmp(ack.type, "RPC_GET_STATUS_ACK"))
+				printf("received RPC_GET_STATUS_ACK\n"), fflush(stdout);
+			inet_close(&local);
+		}
 	}
 
 	printf("waiting for child thread...\n"), fflush(stdout);
 	wait();
 
 	printf("cleaning up...\n"), fflush(stdout);
-	hash_delete(rpc_table);
 
 	return 0;
 }
